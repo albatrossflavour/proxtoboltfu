5070a20aa22d71259ce5e729c110addeeee130d5
Tony Green <tgreen@albatrossflavour.com>
2025-10-12 12:30:05 +1100

    gem

--- site-modules/profile/manifests/pe/primary.pp
+++ site-modules/profile/manifests/pe/primary.pp
@@ -5,6 +5,11 @@
 #
 # Sets up the firewall, backups and puppet strings
 class profile::pe::primary {
+  package { 'toml-rb':
+    provider => 'puppetserver_gem',
+    ensure   => installed,
+    notify => Service['pe-puppetserver'],
+  }
   include profile::pe::autosign
   include profile::pe::agent_types
 }
