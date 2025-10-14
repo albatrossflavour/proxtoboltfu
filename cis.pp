8a846861cb7a1f0be80ed829762975ef4b81b6c1
Tony Green <tgreen@albatrossflavour.com>
2025-10-14 22:16:57 +1100

    Add enable_sce parameter to CIS Enforcement module

---
+++ cis.pp
@@ -0,0 +1,19 @@
+# @summary
+#	  CIS Enforcement
+#
+# @example Basic usage
+#   include profile::soe::cis
+#
+class profile::soe::cis {
+  if lookup('enable_sce',Boolean,first,false) {
+    case $facts['kernel'] {
+      'Linux': {
+        include sce_linux
+      }
+      'windows': {
+        include sce_windows
+      }
+      default: {  }
+    }
+  }
+}
